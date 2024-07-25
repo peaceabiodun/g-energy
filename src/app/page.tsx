546e030daf386a827ba3f6b0c20882f46ed3251a
peaceabiodun <abiodunpeace8@gmail.com>
2024-07-25 16:21:37 +0100

    centered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 const HomePage = () => {
     return ( 
-        <div className="landing_bg w-full h-screen">
-            <div className="p-5 bg-[#3b3b3b67]">
+        <div className="landing_bg w-full h-screen p-10 flex flex-col justify-center">
+            <div className="p-6 bg-[#3b3b3b67] space-y-5 w-[500px] rounded-xl">
                 <h1 className="text-white font-bold text-3xl">
                     Gabskony Renewable Energy
                 </h1>
+                <p className="text-[#eef7ed] text-lg"> Gobal integrated energy company</p>
+                <button className="bg-[#17f3e8] rounded-3xl p-2 w-[160px] h-[40px] text-sm font-semibold">Explore</button>
             </div>
 
         </div>
