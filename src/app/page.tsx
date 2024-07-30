3de151a04b87a39b78e28ff02a8805026835fa64
peaceabiodun <abiodunpeace8@gmail.com>
2024-07-30 15:52:42 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div className="landing_bg w-full h-screen p-10 flex flex-col justify-center">
-      <div className="p-6 bg-[#3b3b3b67] space-y-5 w-[500px] rounded-xl">
+      <div className="p-6 bg-[#3b3b3b67] space-y-5 max-w-[514px] rounded-xl">
         <h1 className="text-white font-bold text-3xl">
           Gabskony Renewable Energy
         </h1>
@@ -14,18 +14,18 @@ const HomePage = () => {
         </button>
       </div>
 
-      <div className="p-6 border w-[80vw] my-10 bg-[#3b3b3b67] rounded-xl flex gap-6">
-        <div>
-            <h2>Solar</h2>
-            <p>Solar energy is all we need. It is good safe and it is renewable</p>
+      <div className="w-full grid grid-cols-3 gap-6 mt-20">
+        <div className="p-6 border bg-[#3b3b3b67] rounded-xl ">
+            <h2 className="text-lg font-semibold text-white mb-4">Solar</h2>
+            <p className="text-[#eef7ed] text-sm">Solar energy is all we need. It is good safe and it is renewable</p>
         </div>
-        <div>
-            <h2>Solar</h2>
-            <p>Solar energy is all we need. It is good safe and it is renewable</p>
+        <div className="p-6 border bg-[#3b3b3b67] rounded-xl ">
+            <h2 className="text-lg font-semibold text-white mb-4">Solar</h2>
+            <p className="text-[#eef7ed] text-sm"> Solar energy is all we need. It is good safe and it is renewable</p>
         </div>
-        <div>
-            <h2>Solar</h2>
-            <p>Solar energy is all we need. It is good safe and it is renewable</p>
+        <div className="p-6 border bg-[#3b3b3b67] rounded-xl ">
+            <h2 className="text-lg font-semibold text-white mb-4">Solar</h2>
+            <p className="text-[#eef7ed] text-sm">Solar energy is all we need. It is good safe and it is renewable</p>
         </div>
       </div>
     </div>
